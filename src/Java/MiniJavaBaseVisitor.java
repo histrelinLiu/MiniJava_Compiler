de608520db6b625b67a9ddfedd2782fefb20a92f
histrelinLiu <15307130083@fudan.edu.cn>
2018-12-10 18:56:39 +0800

    运行冒泡排序成功

--- src/Java/MiniJavaBaseVisitor.java
+++ src/Java/MiniJavaBaseVisitor.java
@@ -29,9 +29,9 @@ public class MiniJavaBaseVisitor<T> extends AbstractParseTreeVisitor<T> implemen
 	private class MyVar{
 		public Type myType;
 		public String name;
-		public int addr;
-		public int length;
-		public MyClass obj, c;
+		public int addr = -1;
+		public int length = 1;
+		public MyClass obj;
 
 		public MyVar(){
 			addr = varsTop++;
@@ -40,44 +40,69 @@ public class MiniJavaBaseVisitor<T> extends AbstractParseTreeVisitor<T> implemen
 			myType = getType(cxt.type());
 			name = cxt.IDENTIFIER().getText();
 			addr = varsTop++;
-			System.out.println("Create a var : "+cxt.getText());
+			//System.out.println("Create a var : "+cxt.type().getText()+" "+cxt.IDENTIFIER().getText());
 		}
 		public MyVar(MiniJavaParser.TypeContext t, MiniJavaParser.IdentifierContext id){
 			myType = getType(t);
 			name = id.IDENTIFIER().getText();
 			addr = varsTop++;
-			System.out.println("Create a var : "+t.getText()+" "+id.getText());
+			//System.out.println("Create a var : "+t.getText()+" "+id.getText());
 		}
-		public MyVar(int length){
-			addr = varsTop;
-			varsTop += length;
-			System.out.println("Create a array");
+		public MyVar(int length1){
+			length = length1;
+			addr = varsTop++;			
+			varsTop += length1;
+			VARS[addr] = addr + 1;
+			//System.out.println("Create a array length = " + String.valueOf(length));
 		}
 		public MyVar(String id){
-			c = findClass(id);
-			obj = new MyClass(c);
-			System.out.println("Create a var : "+id);
+			obj = new MyClass(findClass(id));
+			//System.out.println("Create a var : "+id);
+		}
+		public MyVar(MyVar var){
+			myType = var.myType;
+			name = var.name;
+			length = var.length;
+			if (var.addr == -1){
+				obj = var.obj;
+			}else
+			{
+				addr = varsTop;
+				varsTop += length;
+				for(int i = 0; i < length; i++)
+					VARS[addr + i] = VARS[var.addr + i];
+			}
+
 		}
 	};
 
 	// ClassDeclaration	::=	"class" Identifier ( "extends" Identifier )? "{" ( VarDeclaration )* ( MethodDeclaration )* "}"
 	private class MyClass{
+		public MyClass fa = this;
 		public MyClass extendsClass;
 		public String className;
 		public List<MyMethod> methods = new ArrayList<>();
 		public List<MyVar> vars = new ArrayList<>();
 
 		public MyClass(MiniJavaParser.ClassDeclarationContext cxt){
-			System.out.println("Create MyClass : "+cxt.identifier(0).IDENTIFIER().getText());
+			//System.out.println("Create MyClass : "+cxt.identifier(0).IDENTIFIER().getText());
 			className = cxt.identifier(0).IDENTIFIER().getText();
 			if (cxt.identifier().size() == 2) extendsClass = findClass(cxt.identifier(1).IDENTIFIER().getText());
-
+			for (MiniJavaParser.VarDeclarationContext var : cxt.varDeclaration()) {
+				vars.add(new MyVar(var));
+			}
 			for (MiniJavaParser.MethodDeclarationContext var : cxt.methodDeclaration()) {
 				methods.add(new MyMethod(var, this));
 			}
 		}
 		public MyClass(MyClass c){
-			methods = c.methods;
+			for (MyMethod var : c.methods) {
+				methods.add(var);
+			}
+			for (MyVar var : c.vars) {
+				vars.add(var);
+			}
+			fa = c;
 			className = c.className;
 			extendsClass = c.extendsClass;
 		}
@@ -91,15 +116,16 @@ public class MiniJavaBaseVisitor<T> extends AbstractParseTreeVisitor<T> implemen
 		public List<MyVar> vars = new ArrayList<>();
 		public List<MiniJavaParser.StatementContext> statement = new ArrayList<>();
 		public MiniJavaParser.ExpressionContext returnValue;
+		public int paramNum;
 
 		public MyMethod(MiniJavaParser.MethodDeclarationContext cxt, MyClass c){
-			System.out.println("Create a method : " + cxt.identifier(0).IDENTIFIER().getText());
+			//System.out.println("Create a method : " + cxt.identifier(0).IDENTIFIER().getText());
 			returnType = getType(cxt.type(0));
 			methodName = cxt.identifier(0).IDENTIFIER().getText();
 			
 			// param
+			paramNum = cxt.type().size() - 1;
 			for(int i = 1; i<cxt.type().size(); i++){
-
 				vars.add(new MyVar(cxt.type(i), cxt.identifier(i)));
 			}
 			// var
@@ -108,18 +134,34 @@ public class MiniJavaBaseVisitor<T> extends AbstractParseTreeVisitor<T> implemen
 			for (MiniJavaParser.StatementContext var : cxt.statement()) {
 				statement.add(var);
 			}
-			returnValue = cxt.expression();				
+			returnValue = cxt.expression();		
+
+		}
+		public MyMethod(MyMethod m, MyVar o){
+			//System.out.println("Instance a method : " + m.methodName);
+			returnType = m.returnType;
+			methodName = m.methodName;			
+			obj = o;
+			for (MyVar var : m.vars) 
+				vars.add(new MyVar(var));
+			for (MiniJavaParser.StatementContext var : m.statement) {
+				statement.add(var);
+			}
+			returnValue = m.returnValue;				
 		}
 		
 		public MyVar call(){
 			for (MiniJavaParser.StatementContext var : statement) {
+				for (MyVar param : vars) {
+					//System.out.println("params out :" + param.name + " " + String.valueOf(VARS[param.addr]));
+				}
 				executeStatement(this.obj, this, var);
 			}
 			return executeExpression(this.obj, this, returnValue);
 		}
 	};
 
-	public int[] vars = new int[10000];
+	public int[] VARS = new int[10000];
 	public int varsTop = 0;
 	public List<MyClass> myClasses = new ArrayList<>();
 	public List<MyClass> myObjects = new ArrayList<>();
@@ -156,101 +198,117 @@ public class MiniJavaBaseVisitor<T> extends AbstractParseTreeVisitor<T> implemen
 		System.err.println("ERROR: no method found."+cla.className+"."+id);
 		return null;
 	}
-	public MyVar executeExpression(MyVar obj, MyMethod m, MiniJavaParser.ExpressionContext s){		
-		System.out.println("Execute Statement " + s.getText());
+	public MyVar executeExpression(MyVar obj, MyMethod m, MiniJavaParser.ExpressionContext s){
+		//System.out.println("Execute Expression " + s.getText());
 		MyVar ret = new MyVar();
 		
 		if (s instanceof MiniJavaParser.LoperatorContext) {
 			MyVar v1 = executeExpression(obj, m, ((MiniJavaParser.LoperatorContext)s).expression(0));
 			MyVar v2 = executeExpression(obj, m, ((MiniJavaParser.LoperatorContext)s).expression(1));
-			if      (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.ADD) vars[ret.addr]+=vars[v2.addr];
-			else if (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.SUB) vars[ret.addr]-=vars[v2.addr];
-			else if (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.MUL) vars[ret.addr]*=vars[v2.addr];
-			else if (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.AND) vars[ret.addr]= (vars[v1.addr]==0?false:true) && (vars[v2.addr]==0?false:true)? 1 : 0;
-			else if (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.LT)  vars[ret.addr]= vars[v1.addr] < vars[v2.addr] ? 1 : 0;
+			if      (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.ADD) VARS[ret.addr] = VARS[v1.addr] + VARS[v2.addr];
+			else if (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.SUB) VARS[ret.addr] = VARS[v1.addr] - VARS[v2.addr];
+			else if (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.MUL) VARS[ret.addr] = VARS[v1.addr] * VARS[v2.addr];
+			else if (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.AND) VARS[ret.addr]= (VARS[v1.addr]==0?false:true) && (VARS[v2.addr]==0?false:true)? 1 : 0;
+			else if (((MiniJavaParser.LoperatorContext)s).op.getType() == MiniJavaParser.LT)  VARS[ret.addr]= VARS[v1.addr] < VARS[v2.addr] ? 1 : 0;
 		}
 		else if (s instanceof MiniJavaParser.LarrayContext) {
 			MyVar v1 = executeExpression(obj, m, ((MiniJavaParser.LarrayContext)s).expression(0));
 			MyVar v2 = executeExpression(obj, m, ((MiniJavaParser.LarrayContext)s).expression(1));
-			ret.addr = v1.addr + vars[v2.addr];
-			varsTop--;
+			ret.addr = VARS[v1.addr] + VARS[v2.addr];
 		}
 		else if (s instanceof MiniJavaParser.LlengthContext) {
 			MyVar v1 = executeExpression(obj, m, ((MiniJavaParser.LlengthContext)s).expression());
-			vars[ret.addr] = v1.length;
+			VARS[ret.addr] = v1.length;
 		}
 		else if (s instanceof MiniJavaParser.LmethodContext) {
-			MyVar object = executeExpression(obj, m, ((MiniJavaParser.LmethodContext)s).expression(0));
-			MyMethod method = findMethod(object.c, ((MiniJavaParser.LmethodContext)s).identifier().IDENTIFIER().getText());
-			MyMethod method1 = method;
-			method1.obj = object;
+			MyVar var = executeExpression(obj, m, ((MiniJavaParser.LmethodContext)s).expression(0));
+			//if (var.addr>0) //System.out.println(VARS[var.addr]);
+			MyMethod method = findMethod(var.obj.fa, ((MiniJavaParser.LmethodContext)s).identifier().IDENTIFIER().getText());
+			MyMethod method1 = new MyMethod(method, var);
+			//System.out.println("method's class " + method1.obj.obj.className);
 			for(int i = 1; i< ((MiniJavaParser.LmethodContext)s).expression().size(); i++){
-				method1.vars.add(executeExpression(obj, m, ((MiniJavaParser.LmethodContext)s).expression(i)));
+				MyVar param = new MyVar(executeExpression(obj, m, ((MiniJavaParser.LmethodContext)s).expression(i)));
+				VARS[method1.vars.get(i-1).addr] = VARS[param.addr];
+				method1.vars.get(i-1).obj = param.obj;
 			}
 			ret = method1.call();
 		}
 		else if (s instanceof MiniJavaParser.LintegerContext) {
-			vars[ret.addr] = Integer.parseInt(((MiniJavaParser.LintegerContext)s).Integer().getText());
-			System.out.print("int value ");
-			System.out.println(vars[ret.addr]);
+			VARS[ret.addr] = Integer.parseInt(((MiniJavaParser.LintegerContext)s).Integer().getText());
 		}
 		else if (s instanceof MiniJavaParser.LtrueContext) {
-			vars[ret.addr] = 1;
+			VARS[ret.addr] = 1;
 		}
 		else if (s instanceof MiniJavaParser.LfalseContext) {
-			vars[ret.addr] = 0;
+			VARS[ret.addr] = 0;
 		}
 		else if (s instanceof MiniJavaParser.LidentifierContext) {
 			ret = findvar(obj, m, ((MiniJavaParser.LidentifierContext)s).identifier().IDENTIFIER().getText());
 		}
 		else if (s instanceof MiniJavaParser.LnewarrayContext) {
 			MyVar v1 = executeExpression(obj, m, ((MiniJavaParser.LnewarrayContext)s).expression());
-			ret = new MyVar(vars[v1.addr]);
+			ret = new MyVar(VARS[v1.addr]);
 		}
 		else if (s instanceof MiniJavaParser.LnewobjectContext) {
-			ret = new MyVar((String)(((MiniJavaParser.LnewobjectContext)s).identifier().IDENTIFIER().getText()));
+			ret = new MyVar(((MiniJavaParser.LnewobjectContext)s).identifier().IDENTIFIER().getText());
 		}
 		else if (s instanceof MiniJavaParser.LbangContext) {
 			MyVar v1 = executeExpression(obj, m, ((MiniJavaParser.LbangContext)s).expression());
-			vars[ret.addr] = ((vars[v1.addr]==0)?true:false)?1:0;
+			VARS[ret.addr] = ((VARS[v1.addr]==0)?true:false)?1:0;
 		}
 		else if (s instanceof MiniJavaParser.LsubexpressionContext) {
 			MyVar v1 = executeExpression(obj, m, ((MiniJavaParser.LsubexpressionContext)s).expression());
-			vars[ret.addr] = vars[v1.addr];
+			VARS[ret.addr] = VARS[v1.addr];
 		}
 		else if (s instanceof MiniJavaParser.LthisContext) {
 			ret = obj;
+			//System.out.println("Execute Expression " + s.getText() + ", result = " + ret.obj.className);
 		}
+		//System.out.println("Execute Expression " + s.getText() + ", result = " + ((ret.addr<10000 && ret.addr>=0)?(String.valueOf(VARS[ret.addr])):ret.obj.className));
 		return ret;
 	}
 	public int executeStatement(MyVar obj, MyMethod m, MiniJavaParser.StatementContext s){
+		/*if (obj!=null)
+		{
+			System.out.print(obj.obj.className + " vars out ");
+			for (MyVar var : obj.obj.vars) {
+				if (var.addr!=-1) System.out.print(var.name + "=" + VARS[var.addr] + "; ");
+				else System.out.print(var.name + "=" + var.obj.fa.className);
+			}
+		}*/
+		//System.out.println(" ");
+		//System.out.println("Execute Statement " + s.getText());
 		if (s instanceof MiniJavaParser.LsubstatementContext) {
 			for (MiniJavaParser.StatementContext var : ((MiniJavaParser.LsubstatementContext)s).statement()) {
 				executeStatement(obj, m, var);
 			}
 		}
 		else if (s instanceof MiniJavaParser.LifContext) {
-			if (vars[executeExpression(obj, m, ((MiniJavaParser.LifContext)s).expression()).addr] != 0) 
+			if (VARS[executeExpression(obj, m, ((MiniJavaParser.LifContext)s).expression()).addr] != 0) 
 				executeStatement(obj, m, ((MiniJavaParser.LifContext)s).statement(0));
 			else 
 				executeStatement(obj, m, ((MiniJavaParser.LifContext)s).statement(1));
 		}
 		else if (s instanceof MiniJavaParser.LwhileContext) {
-			while(vars[executeExpression(obj, m, ((MiniJavaParser.LwhileContext)s).expression()).addr] != 0){
+			while(VARS[executeExpression(obj, m, ((MiniJavaParser.LwhileContext)s).expression()).addr] != 0){
 				executeStatement(obj, m, ((MiniJavaParser.LwhileContext)s).statement());
 			}
 		}
 		else if (s instanceof MiniJavaParser.LprintContext) {
-			System.out.println(vars[executeExpression(obj, m, ((MiniJavaParser.LprintContext)s).expression()).addr]);
+			System.
+			out.println(VARS[executeExpression(obj, m, ((MiniJavaParser.LprintContext)s).expression()).addr]);
 		}
 		else if (s instanceof MiniJavaParser.LassignContext) {
 			MyVar v = findvar(obj, m, ((MiniJavaParser.LassignContext)s).identifier().IDENTIFIER().getText());
-			vars[v.addr] = vars[executeExpression(obj, m, ((MiniJavaParser.LassignContext)s).expression()).addr];
+			if (v.addr == -1)
+				v.obj = executeExpression(obj, m, ((MiniJavaParser.LassignContext)s).expression()).obj;
+			else 
+				VARS[v.addr] = VARS[executeExpression(obj, m, ((MiniJavaParser.LassignContext)s).expression()).addr];
 		}
 		else if (s instanceof MiniJavaParser.LarrayassignContext) {
 			MyVar v = findvar(obj, m, ((MiniJavaParser.LarrayassignContext)s).identifier().IDENTIFIER().getText());
-			vars[v.addr + vars[executeExpression(obj, m, ((MiniJavaParser.LarrayassignContext)s).expression(0)).addr]]
-				  = vars[executeExpression(obj, m, ((MiniJavaParser.LarrayassignContext)s).expression(1)).addr];
+			VARS[VARS[v.addr] + VARS[executeExpression(obj, m, ((MiniJavaParser.LarrayassignContext)s).expression(0)).addr]]
+				  = VARS[executeExpression(obj, m, ((MiniJavaParser.LarrayassignContext)s).expression(1)).addr];
 		}
 		else {
 			System.err.println("ERROR: Statement type error, none statement be executed."); 
